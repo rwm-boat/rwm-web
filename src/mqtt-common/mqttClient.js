e118b5d51b3d8a12867f6546af39b07673f3ff87
Andrew Robbertz <andrewrobbertz@Andrews-MacBook-Pro.local>
2020-03-10 21:20:16 -0400

    prototype website

--- src/mqtt-common/mqttClient.js
+++ src/mqtt-common/mqttClient.js
@@ -1,31 +1,25 @@
-// Import Paho
 import Paho from 'paho-mqtt'
 
-var client = new Paho.Client('192.168.1.170', 9001, 'web-client');
-// Set Callback handlers 
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-client.onConnected = onConnected;
-// Connect to the Broker
-connect(true)
-// Subscribe To some topics
-subscribe_to_topic('/status/compass')
+const broker_ip = '192.168.1.170';
+const broker_port = 9001
 
-
-
-async function connect(tryReconnect) {
-    // Connect the Client
+// Creates an MQTT client and connects to the broker service. 
+function connect(tryReconnect, topic) {
+    const client = new Paho.Client(broker_ip, broker_port, 'web-client');
     console.log('Trying to connecct to broker')
-    await client.connect({
+    client.connect({
         cleanSession: true,
         reconnect: tryReconnect, 
+        onSuccess: () => {
+            console.log('Connected to Broker at : ' + broker_ip);
+            client.subscribe(topic, {onSuccess: () => {
+                console.log('Subscribed to topic : ' + topic)
+            }});
+        }
     });
-}
-
-// Called when the client connectes
-function onConnected (reconnect, uri) {
-    if (reconnect) console.log('Reconnected to Broker at : ' + uri);
-    else console.log('Connected to new Broker at : ' + uri);
+    // Set Callback handlers 
+    client.onConnectionLost = onConnectionLost;
+    return client;
 }
 
 // Callled when the client loses it's connection 
@@ -35,23 +29,24 @@ function onConnectionLost (responseObject) {
     }
 }
 
-function subscribe_to_topic (topic) {
+
+function subscribe_to_topic (client, topic) {
     client.subscribe(topic, {onSuccess: () => {
         console.log('Subscribed to topic : ' + topic)
     }});
 }
 
-function pubish_message (topic, msg) {
+function pubish_message (client, topic, msg) {
     var message = new Paho.Message(msg);
     message.destinationName = topic;
     client.send(message);
 }
 
-// Called when a Message arrives
-function onMessageArrived (message) {
-    console.log('onMessageArrived:' + message.payloadString)
+
+const mqttService = {
+    connect,
+    subscribe_to_topic,
+    pubish_message,
 }
 
-
-
-
+export default mqttService; 
